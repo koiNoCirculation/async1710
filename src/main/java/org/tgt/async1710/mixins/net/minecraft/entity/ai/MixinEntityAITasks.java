68356cfb35217a583d1b8124d408fff7832cd15a
koiNoCirculation <belica.ly42@gmail.com>
2021-01-20 09:50:41 +0800

    update

--- src/main/java/org/tgt/async1710/mixins/net/minecraft/entity/ai/MixinEntityAITasks.java
+++ src/main/java/org/tgt/async1710/mixins/net/minecraft/entity/ai/MixinEntityAITasks.java
@@ -1,6 +1,7 @@
 package org.tgt.async1710.mixins.net.minecraft.entity.ai;
 
 import io.netty.util.internal.ConcurrentSet;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAITasks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -28,34 +29,6 @@ public abstract class MixinEntityAITasks {
     public <E> boolean taskEntries_addTask(List<E> list, E e) {
         return taskEntriesSet.add((EntityAITasks.EntityAITaskEntry) e);
     }
-
-    @Redirect(method = "removeTask", at = @At(value = "INVOKE", target = "Ljava/util/List;iterator()Ljava/util/Iterator;", remap = false))
-    public Iterator taskEntries_removeTask(List list) {
-        return taskEntriesSet.iterator();
-    }
-
-
-    @Redirect(method = "canUse", at = @At(value = "INVOKE", target = "Ljava/util/List;iterator()Ljava/util/Iterator;", remap = false))
-    private Iterator taskEntries_canUse(List list) {
-        return taskEntriesSet.iterator();
-    }
-
-    @Redirect(method = "removeTask", at = @At(value = "INVOKE", target = "Ljava/util/List;contains(Ljava/lang/Object;)Z", remap = false))
-    public boolean executingTaskEntries_contains_removeTask(List list, Object o) {
-        return executingTaskEntriesSet.contains(o);
-    }
-
-    @Redirect(method = "removeTask", at = @At(value = "INVOKE", target = "Ljava/util/List;remove(Ljava/lang/Object;)Z", remap = false))
-    public boolean executingTaskEntries_remove_removeTask(List list, Object o) {
-        return executingTaskEntriesSet.remove( o);
-    }
-
-
-    @Redirect(method = "canUse", at = @At(value = "INVOKE", target = "Ljava/util/List;contains(Ljava/lang/Object;)Z", remap = false))
-    private boolean executingTaskEntries_contains_canUse(List list, Object o) {
-        return executingTaskEntriesSet.contains(o);
-    }
-
 
     /**
      * @author
@@ -90,7 +63,7 @@ public abstract class MixinEntityAITasks {
         }
         else
         {
-            ((ReadWriteLockedSet<EntityAITasks.EntityAITaskEntry>)executingTaskEntriesSet).foreachWithRemove(
+            executingTaskEntriesSet.foreachWithRemove(
                     entityAITaskEntry -> {}, entityAITaskEntry -> entityAITaskEntry.action.continueExecuting()
             );
         }
@@ -132,4 +105,23 @@ public abstract class MixinEntityAITasks {
         });
         return true;
     }
+
+    /**
+     * @author
+     */
+    @Overwrite
+    public void removeTask(EntityAIBase p_85156_1_)
+    {
+        taskEntriesSet.foreachWithRemove(entityaitaskentry -> {
+            if (entityaitaskentry.action == p_85156_1_)
+            {
+                if (executingTaskEntriesSet.contains(entityaitaskentry))
+                {
+                    entityaitaskentry.action.resetTask();
+                    executingTaskEntriesSet.remove(entityaitaskentry);
+                }
+            }
+        }, (entityaitaskentry) -> entityaitaskentry.action == p_85156_1_);
+    }
+
 }
